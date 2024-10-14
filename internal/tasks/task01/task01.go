2612ddb7b864efdb78fd93634a100656fcc341b6
Mindaugas <mindaugas932@gmail.com>
2024-10-15 00:05:01 +0300

    Task 01A

--- internal/tasks/task01/task01.go
+++ internal/tasks/task01/task01.go
@@ -1,15 +1,41 @@
 package task01
 
 import (
+	"github.com/mindaugasw/advent-of-code-2023-golang/internal"
 	"github.com/mindaugasw/advent-of-code-2023-golang/internal/tasks"
+	"regexp"
+	"strconv"
 )
 
 func init() {
 	tasks.Register(1, "A", SolveA)
 }
 
-func SolveA() string {
-	msg := "Solving task 01"
+func SolveA(input string) (result string, err error) {
+	lines, err := internal.ReadLines(input)
+	if err != nil {
+		return
+	}
 
-	return msg
+	pattern := regexp.MustCompile("\\d")
+	sum := 0
+
+	for _, line := range lines {
+		matches := pattern.FindAllString(line, -1)
+
+		if len(matches) == 0 {
+			continue
+		}
+
+		lineDigits := matches[0] + matches[len(matches)-1]
+		lineNumber, err := strconv.Atoi(lineDigits)
+
+		if err != nil {
+			return "", err
+		}
+
+		sum += lineNumber
+	}
+
+	return strconv.Itoa(sum), nil
 }
