814296185ebb1798e68b013d62743e664fbae830
Mindaugas <mindaugas932@gmail.com>
2024-10-15 23:40:24 +0300

    Task 01B

--- internal/tasks/task01/task01.go
+++ internal/tasks/task01/task01.go
@@ -5,10 +5,24 @@ import (
 	"github.com/mindaugasw/advent-of-code-2023-golang/internal/tasks"
 	"regexp"
 	"strconv"
+	"strings"
 )
+
+var replaceMap = []string{
+	"one", "o1e",
+	"two", "t2o",
+	"three", "t3e",
+	"four", "f4r",
+	"five", "f5e",
+	"six", "s6x",
+	"seven", "s7n",
+	"eight", "e8t",
+	"nine", "n9e",
+}
 
 func init() {
 	tasks.Register(1, "A", SolveA)
+	tasks.Register(1, "B", SolveB)
 }
 
 func SolveA(input string) (result string, err error) {
@@ -22,6 +36,42 @@ func SolveA(input string) (result string, err error) {
 
 	for _, line := range lines {
 		matches := pattern.FindAllString(line, -1)
+
+		if len(matches) == 0 {
+			continue
+		}
+
+		lineDigits := matches[0] + matches[len(matches)-1]
+		lineNumber, err := strconv.Atoi(lineDigits)
+
+		if err != nil {
+			return "", err
+		}
+
+		sum += lineNumber
+	}
+
+	return strconv.Itoa(sum), nil
+}
+
+func SolveB(input string) (result string, err error) {
+	lines, err := internal.ReadLines(input)
+	if err != nil {
+		return
+	}
+
+	replacer := strings.NewReplacer(replaceMap...)
+	pattern := regexp.MustCompile("\\d")
+	sum := 0
+
+	for _, line := range lines {
+		// Here we fix overlapping numbers (e.g. oneight). Because regex matches only the first one,
+		// but for the task this is valid as "18".
+		// So we replace it "oneight" (original) => "o1eight" (1st replace) => "o1e8t" (2nd replace)
+		// to allow matching both 1 and 8.
+		lineReplaced := replacer.Replace(line)
+		lineReplaced = replacer.Replace(lineReplaced)
+		matches := pattern.FindAllString(lineReplaced, -1)
 
 		if len(matches) == 0 {
 			continue
--- internal/tasks_test/solutions_test.go
+++ internal/tasks_test/solutions_test.go
@@ -1,8 +1,12 @@
 package tasks_test
 
 import (
+	"fmt"
 	"github.com/mindaugasw/advent-of-code-2023-golang/internal/tasks"
 	"testing"
+
+	// Tasks self-register using the registry and init()
+	_ "github.com/mindaugasw/advent-of-code-2023-golang/internal/tasks/task01"
 )
 
 func TestSolutionProvider(t *testing.T) {
@@ -12,25 +16,32 @@ func TestSolutionProvider(t *testing.T) {
 		input    string
 		expected string
 	}{
+		{1, "A", "exampleA.txt", "142"},
 		{1, "A", "input.txt", "54927"},
-		{1, "A", "example.txt", "142"},
+		{1, "B", "exampleB.txt", "281"},
+		{1, "B", "input.txt", "54581"},
 	}
 
 	for _, set := range dataSets {
-		taskFunc, ok := tasks.Get(set.number, set.part)
+		testName := fmt.Sprintf("%d%s-%s", set.number, set.part, set.input)
+		t.Run(
+			testName, func(t *testing.T) {
+				taskFunc, ok := tasks.Get(set.number, set.part)
 
-		if !ok {
-			t.Fatalf("Could not get taskFunc")
-		}
+				if !ok {
+					t.Fatalf("Could not get taskFunc")
+				}
 
-		result, err := taskFunc(set.input)
+				result, err := taskFunc(set.input)
 
-		if err != nil {
-			t.Fatalf("Error returned: %v", err)
-		}
+				if err != nil {
+					t.Fatalf("Error returned: %v", err)
+				}
 
-		if result != set.expected {
-			t.Fatalf("Incorrect result. Expected '%v', got '%v'", set.expected, result)
-		}
+				if result != set.expected {
+					t.Fatalf("Incorrect result. Expected '%v', got '%v'", set.expected, result)
+				}
+			},
+		)
 	}
 }
